0073e337d45d040dcfafcf58af2930ba46231d8c
Hanna-boxdigi <funnyboxdigi@gmail.com>
2022-10-05 17:09:39 +0300

    correct mistake in div's className

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <Weather defaultCity="Kyiv" />
       <footer>
         <p>coded by <a href="https://teal-speculoos-2e6228.netlify.app/" target="_blank" rel="noreferrer">Hanna</a> and is <a href="https://github.com/boxdigi/weather-react-project" target="_blank" rel="noreferrer">open-sourced</a></p>
-        <div class="d-flex justify-content-evenly">
+        <div className="d-flex justify-content-evenly">
           <p>inspired by <a href="https://dribbble.com/shots/2427734-Black-White-Weather" target="_blank" rel="noreferrer">Emil Widlund
           </a></p>
           <p>used <a href="https://openweathermap.org/">OpenWeatherMap</a></p></div>
